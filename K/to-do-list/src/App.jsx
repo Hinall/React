6d82ab69f66dc36454493d816b4093954cf4586a
hinal <102239391+Hinall@users.noreply.github.com>
2024-09-15 01:20:46 +0530

    useReduce

--- K/to-do-list/src/App.jsx
+++ K/to-do-list/src/App.jsx
@@ -2,43 +2,33 @@ import React from "react";
 import AppName from "./Components/AppName";
 import AddTask from "./Components/AddTask";
 import TaskItem from "./Components/TaskItem";
-import { useState } from "react";
-import { AllItemsContext } from "./store/AllItems";
 
 function App() {
-  const [Items, setItem] = useState([]);
+  // const addItems = (a, b) => {
+  //   console.log(`new item added ${a} and ${b}`);
+  //   // const newItem = [...Items, { taskname: a, date: b }];// storing in  constant and then
+  //   // storing is not advisable due asyn function calling in background instead take current value only
+  //   setItem((current) => [...current, { taskname: a, date: b }]); //use functional update to avoid Asynchronous updates
+  // };
 
-  const addItems = (a, b) => {
-    console.log(`new item added ${a} and ${b}`);
-    // const newItem = [...Items, { taskname: a, date: b }];// storing in  constant and then
-    // storing is not advisable due asyn function calling in background instead take current value only
-    setItem((current) => [...current, { taskname: a, date: b }]); //use functional update to avoid Asynchronous updates
-  };
-
-  const deleteItem = (taskname) => {
-    console.log("delete item " + taskname);
-    const ItemAfterDeletion = Items.filter(
-      (item) => item.taskname !== taskname
-    );
-    setItem(ItemAfterDeletion);
-  };
+  // const deleteItem = (taskname) => {
+  //   console.log("delete item " + taskname);
+  //   const ItemAfterDeletion = Items.filter(
+  //     (item) => item.taskname !== taskname
+  //   );
+  //   setItem(ItemAfterDeletion);
+  // };
   return (
     <>
       <center>
-        <AllItemsContext.Provider
-          value={{
-            Items: Items,
-            addItem: addItems,
-            deleteItem: deleteItem,
-          }}
-        >
+        <contextProvider>
           <AppName />
           <div className="container">
             <AddTask />
-            {Items.length === 0 && <h3>welocome</h3>}
+            {/* {Items.length === 0 && <h3>welocome</h3>} */}
             <TaskItem />
           </div>
-        </AllItemsContext.Provider>
+        </contextProvider>
       </center>
     </>
   );
--- K/to-do-list/src/store/AllItems.jsx
+++ K/to-do-list/src/store/AllItems.jsx
@@ -1,3 +1,55 @@
 import { createContext } from "react";
-
+import { useReducer } from "react";
 export const AllItemsContext = createContext([]);
+
+const contextProvider = ({ children }) => {
+  const [Items, dispatchItem] = useReducer(reducerfn, []); //
+
+  const addItems = (taskname, tdate) => {
+    const ItemAction = {
+      type: "ADD",
+      payload: {
+        taskname: taskname,
+        date: tdate,
+      },
+    };
+    dispatchItem(ItemAction);
+  };
+
+  const deleteItem = (taskname) => {
+    const ItemAction = {
+      type: "DELETE",
+      payload: {
+        taskname: taskname,
+      },
+    };
+    dispatchItem(ItemAction);
+  };
+
+  const reducerfn = (currentstate, action) => {
+    if (action.type === "ADD") {
+      return [
+        ...currentstate,
+        { taskname: action.payload.taskname, date: action.payload.date },
+      ];
+    } else if (action.type === "DELETE") {
+      return currentstate.filter(
+        (item) => item.taskname !== action.payload.taskname
+      );
+    }
+    return currentstate;
+  };
+
+  return (
+    <AllItemsContext.Provider
+      value={{
+        Items: Items,
+        addItem: addItems,
+        deleteItem: deleteItem,
+      }}
+    >
+      {children}
+    </AllItemsContext.Provider>
+  );
+};
+export default contextProvider;
